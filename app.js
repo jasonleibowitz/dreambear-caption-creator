9b891172818abd65a73c83614b5445f7b23fb822
Jason Leibowitz <jason.leibowitz@tigerspike.com>
2015-08-16 18:27:07 -0400

    upload original file to s3. working on getting imagemagick to upload to s3

--- app.js
+++ app.js
@@ -3,57 +3,85 @@ var im = require('imagemagick');
 var path = require('path');
 var fs = require('fs');
 var multer = require('multer');
-// var multer = multer({ dest: 'public/uploads/'});
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads/raw')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-})
-var upload = multer({ storage: storage })
+var s3 = require('multer-s3');
+var AWS = require('aws-sdk');
+
+var accessKeyId = process.env.AWS_ACCESS_KEY || "AKIAJFKOBOO2UC2YJ57A";
+var secretAccessKey = process.env.AWS_SECRET_KEY || "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";
 
 var app = express();
+AWS.config.loadFromPath('./aws-config.json');
+var s3bucket = new AWS.S3({params: {Bucket: 'dreambear-captions', Body: 'EXPECTED CONTENTS'}});
 
-// app.use(multer({dest: './public/uploads'}).single('photo'));
+const AWS_PATH = 'https://s3.amazonaws.com/dreambear-captions/';
+
+// multer-s3
+var upload = multer({
+    storage: s3({
+        dirname: './raw/',
+        bucket: 'dreambear-captions',
+        secretAccessKey: "fZhQOYRyGUdWPIPAekX4d4acHKfh8InXloJM7GXW",
+        accessKeyId: "AKIAJFKOBOO2UC2YJ57A",
+        region: 'us-east-1',
+    })
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view enginer', 'ejs');
 app.use("/images", express.static(__dirname + '/public/images'));
 app.use("/uploads", express.static(__dirname + '/public/uploads'));
 
-var form = "<!DOCTYPE HTML><html><body>" +
-"<form method='post' action='/upload' enctype='multipart/form-data'>" +
-"<input type='file' name='photo'/>" + "<input type='text' name='textCaption' /> " +
-"<input type='submit' /></form>" +
-"</body></html>";
-
 app.get('/', function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(form);
+	// res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log('path: ' + AWS_PATH);
+
+	res.render('index.ejs');
 });
 
 app.post('/upload', upload.single('photo'), function(req, res) {
-	// console.log(req.file);
-	im.crop({
-		srcPath: 'public/uploads/raw/' + req.file.originalname,
-		dstPath: 'public/uploads/tmp/' + req.file.originalname,
+	// console.log('inside upload:', req.file);
+    var filename = req.file.originalname;
+
+	// im.crop({
+	// 	srcPath: AWS_PATH + 'raw/' + filename,
+	// 	dstPath: AWS_PATH + 'tmp/' + + filename,
+	// 	width: 366,
+	// 	height: 244,
+	// 	quality: 1,
+	// 	gravity: 'Center'
+	// }, function(err, stdout, stderr) {
+	// 	if (err) throw err;
+	// 	im.convert([AWS_PATH + 'temp/' + filename, '-gravity', 'South', '-fill', 'rgba(0,0,0,.5)', '-draw', 'rectangle +0+170+366+244', '-fill', 'white', '-font', 'Helvetica', '-gravity', 'South', '-pointsize', '15', '-annotate', '+0+30', req.body.textCaption, AWS_PATH + 'final/' + filename], function(err, stdout) {
+	// 		if (err) throw err;
+	// 		// fs.unlinkSync("public/uploads/tmp/" + req.file.originalname);
+	// 		// fs.unlinkSync("public/uploads/raw/" + req.file.originalname);
+	// 		res.redirect('uploads/final/' + req.file.originalname)
+	// 	});
+	// });
+
+    im.crop({
+		srcPath: AWS_PATH + 'raw/' + filename,
+		dstPath: 'uploads/tmp/test',
 		width: 366,
 		height: 244,
 		quality: 1,
 		gravity: 'Center'
 	}, function(err, stdout, stderr) {
 		if (err) throw err;
-		im.convert(['public/uploads/tmp/' + req.file.originalname, '-gravity', 'South', '-fill', 'rgba(0,0,0,.5)', '-draw', 'rectangle +0+170+366+244', '-fill', 'white', '-font', 'Helvetica', '-gravity', 'South', '-pointsize', '15', '-annotate', '+0+30', req.body.textCaption, 'public/uploads/final/' + req.file.originalname], function(err, stdout) {
-			if (err) throw err;
-			// console.log('stdout: ', stdout);
-			// res.render("index.ejs");
-			fs.unlinkSync("public/uploads/tmp/" + req.file.originalname);
-			fs.unlinkSync("public/uploads/raw/" + req.file.originalname);
-			res.redirect('uploads/final/' + req.file.originalname)
-		});
+
+        s3bucket.putObject({
+            Key: 'tmp/test.jpg',
+            Body: new Buffer(stdout, 'binary'),
+            ContentType: 'image/jpeg'
+        }, function(err, data) {
+            if (err) {
+                console.log('Failing to save to S3: ', err);
+            } else {
+                console.log('uploaded to s3');
+            }
+        })
 	});
+
 
 });
 
